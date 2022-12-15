6984082d19387639bdaecc34cca1b46aa57791df
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-12-14 22:59:12 -0500

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Installation instructions
+
+Create virutal environment:
+
+```bash
+python -m venv ./venv
+```
+
+Activate the virtual environment:
+
+```bash
+.\venv\Scripts\activate
+```
+
+Install the requirements:
+
+```bash
+pip install -r requirements.txt
+```
+
+Run the application:
+
+```bash
+python server.py
+```
---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from json import dumps
+from flask_cors import CORS
+import os
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = './images/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+app.config['DEBUG'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+cors = CORS(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/image/', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return dumps({"message": 'No file part'})
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return dumps({"message": 'No selected file'})
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # return redirect(url_for('download_file', name=filename))
+        return  dumps({"message": 'Image saved'})
+    return dumps({"message": 'File not allowed'})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
