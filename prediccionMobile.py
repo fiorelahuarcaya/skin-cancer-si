fd50a58ca27e4ed996ef5c08b988b0ce8a663e97
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-12-18 00:01:10 -0500

    prediction

---
+++ prediccionMobile.py
@@ -0,0 +1,85 @@
+# Importing the libraries
+import keras
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D, Flatten, BatchNormalization, Activation, Dropout
+from keras.callbacks import ModelCheckpoint, TensorBoard
+import keras.utils as image               
+from tqdm import tqdm
+from PIL import ImageFile                            
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import numpy as np
+import pandas as pd
+import os
+from sklearn.datasets import load_files
+from keras.utils import np_utils
+#import keras.utils as image     
+
+def path_to_tensor(img_path):
+    """
+    Getting a tensor from a given path.
+    """
+    # Loading the image
+    img = image.load_img(img_path, target_size=(512, 512))
+    # Converting the image to numpy array
+    x = image.img_to_array(img)   
+    # convert 3D tensor to 4D tensor with shape (1, 512, 512, 3)
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    """
+    # Getting a list of tensors from a given path directory.
+    """
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+def mobilenet_architecture():
+    """
+    Pre-build architecture of mobilenet for our dataset.
+    """
+    # Imprting the model
+    from keras.applications.mobilenet import MobileNet
+
+    # Pre-build model
+    base_model = MobileNet(include_top = False, weights = None, input_shape = (512, 512, 3))
+
+    # Adding output layers
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    output = Dense(units = 2, activation = 'softmax')(x)
+
+    # Creating the whole model
+    mobilenet_model = Model(base_model.input, output)
+    
+    # Getting the summary of architecture
+    #mobilenet_model.summary()
+    
+    # Compiling the model
+    mobilenet_model.compile(optimizer = keras.optimizers.Adam(lr = 0.001), 
+                            loss = 'categorical_crossentropy', 
+                            metrics = ['accuracy'])
+
+    return mobilenet_model
+
+
+model_architecture = mobilenet_architecture()
+# Aquí se pone el modelo de la red neuronal hdf5
+weight_path = "./weights.best.mobilenet.hdf5"
+
+
+def predict(img_path, 
+            model_architecture = model_architecture, 
+            path_model_weight = weight_path):
+    # Getting the tensor of image
+    image_to_predict = path_to_tensor(img_path).astype('float32')/255
+    # Getting the model's architecture
+    model = model_architecture
+    # Loading the weights
+    model.load_weights(path_model_weight)
+    # Predicting
+    pred = model.predict(image_to_predict)
+    # En la variable pred[0][0] está el valor que toma si es melanoma
+    # En la variable pred[0][1] está el valor que toma si no es melanoma
+    # El valor más alto es al que representa.
+    print("Prediction..." + " Melanoma : ", pred[0][0], " | Other : ", pred[0][1])
+    
+    return [float(np.argmax(pred)) , float(pred[0][0]) , float(pred[0][1])]
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from json import dumps
 from flask_cors import CORS
 import os
 from werkzeug.utils import secure_filename
-
+from prediccionMobile import predict
 
 UPLOAD_FOLDER = './images/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -32,7 +32,9 @@ def upload_file():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # return redirect(url_for('download_file', name=filename))
-        return  dumps({"message": 'Image saved'})
+        result = predict("./images/"+filename)
+
+        return  dumps({"predict": result[0] , "melanoma": result[1] , "other": result[2]})
     return dumps({"message": 'File not allowed'})
 
 if __name__ == "__main__":
